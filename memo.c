44f63ae55d33b046b1e52badebde594a9dc00c32
azuma-ya <kazu.bluemountain@gmail.com>
2023-06-12 15:24:47 +0900

    refactor: #1 文字の変換指定について検証する

--- memo.c
+++ memo.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+int str_char(char s[], char c){
+  printf("%c\n",c);
+  printf("%d\n",s[0]);
+  for (int i = 0; s[i] != '\0'; i++){
+    if(s[i] == c){
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main(void){
-  for (int i = 0; i < 5; i++){
-    printf("a");
-  }printf("\n");
+  int no;
+  char ch[10];
+
+  printf("英文字を入力せよ:");
+  scanf("%s",ch);
+
+  // printf("%c",ch[0]);
+  no = str_char("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", ch[0]) +1;
+
+  printf("%d\n", no);
   return 0;
 }
